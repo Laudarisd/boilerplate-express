ff1f24aa7e701b1ca7f58f265b017f36af2fdd6d
laudarisd <sudiplaudari@gmail.com>
2021-10-07 11:46:43 +0900

    Update myApp

--- myApp.js
+++ myApp.js
@@ -60,7 +60,18 @@ app.get("/json", function(req, res){
 
 
 /** 8) Chaining middleware. A Time server */
-
+app.get(
+    "/now",
+    (req, res, next) => {
+      req.time = new Date().toString();
+      next();
+    },
+    (req, res) => {
+      res.send({
+        time: req.time
+      });
+    }
+  );
 
 /** 9)  Get input from client - Route parameters */
 
